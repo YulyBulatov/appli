247d958e8e624c9107efc740a206dcbce8532293
YulyBulatov <yulik.bulatov@gmail.com>
2022-11-30 10:58:12 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+    session_start();
+    require_once "functions.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+        <title>Ajout produit</title>
+    </head>
+    <body>
+
+        <h1>Ajouter un produit</h1>
+        <form action="traitement.php?action=add" method="post">
+            <p>
+                <label>
+                    Nom de produit :
+                    <input type="text" name="name">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix de produit :
+                    <input type="number" step="any" name="price">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée :
+                    <input type="number" name="qtt" value="1">
+                </label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Ajouter le produit">
+            </p>
+        </form>
+
+        <h2>Le nombre de produits en session:</h2>
+        <p>
+            <?php
+            if (empty($_SESSION['products'])){
+                echo "0";
+            }
+            else{
+                echo count($_SESSION['products']);
+            }
+            ?>
+        </p>
+
+        <nav>
+            <?= getMessages() ?>
+            <a href="/appli">Page d'accueil</a>
+            <a href="recap.php/">Recapitulatif</a>
+        </nav>
+
+    </body>
+</html>
---
+++ traitement.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+
+    if(isset($_GET['action'])){
+
+        switch($_GET['action']){
+
+            case "add":
+
+    if(isset($_POST['submit'])){
+
+        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+
+        if($name && $price && $qtt){
+
+            $product = [
+                "name"  => $name,
+                "price" => $price,
+                "qtt"   => $qtt,
+                "total" => $price*$qtt
+            ];
+
+            $_SESSION['products'][] = $product;
+            $_SESSION["message"] = "Produit enregistré avec succès !";
+        }
+
+    }
+
+            case "delete":
+
+                
+}
+}
+
+
+    header("Location:index.php");
